148316fbfca6c3c88274ad79e1010310c6a3749b
IMS212 <ims@irisshaders.dev>
2026-03-24 09:10:42 -0700

    Bump version to 0.8.7

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -1,17 +1,17 @@
  import org.gradle.api.Project
 
 object BuildConfig {
-    val MINECRAFT_VERSION: String = "26.1-rc-3"
+    val MINECRAFT_VERSION: String = "26.1"
     val NEOFORGE_VERSION: String = "26.1.0.0-alpha.0+rc-3.20260323.185344"
     val FABRIC_LOADER_VERSION: String = "0.18.4"
-    val FABRIC_API_VERSION: String = "0.143.12+26.1"
+    val FABRIC_API_VERSION: String = "0.144.0+26.1"
     val SUPPORT_FRAPI : Boolean = false
 
     // This value can be set to null to disable Parchment.
     val PARCHMENT_VERSION: String? = null
 
     // https://semver.org/
-    var MOD_VERSION: String = "0.8.7-beta.1"
+    var MOD_VERSION: String = "0.8.7"
 
     fun createVersionString(project: Project): String {
         val builder = StringBuilder()
