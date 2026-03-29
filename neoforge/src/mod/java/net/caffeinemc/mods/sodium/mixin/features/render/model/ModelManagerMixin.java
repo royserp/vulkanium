eab0454bb32462c7248ebc7a1f7d2f9600ee4001
AR <1516775492@qq.com>
2026-03-30 01:41:53 +0800

    Fix item model fix crashes on neo (#3564)

---
+++ neoforge/src/mod/java/net/caffeinemc/mods/sodium/mixin/features/render/model/ModelManagerMixin.java
@@ -0,0 +1,20 @@
+package net.caffeinemc.mods.sodium.mixin.features.render.model;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.caffeinemc.mods.sodium.client.render.immediate.model.ImprovedItemModelBuilder;
+import net.minecraft.client.resources.model.ModelDiscovery;
+import net.minecraft.client.resources.model.ModelManager;
+import net.minecraft.client.resources.model.UnbakedModel;
+import net.minecraft.resources.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(ModelManager.class)
+public class ModelManagerMixin {
+
+    @WrapOperation(method = "discoverModelDependencies(Ljava/util/Map;Lnet/minecraft/client/resources/model/BlockStateModelLoader$LoadedModels;Lnet/minecraft/client/resources/model/ClientItemInfoLoader$LoadedClientInfos;Lnet/neoforged/neoforge/client/model/standalone/StandaloneModelLoader$LoadedModels;)Lnet/minecraft/client/resources/model/ModelManager$ResolvedModels;", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/resources/model/ModelDiscovery;addSpecialModel(Lnet/minecraft/resources/Identifier;Lnet/minecraft/client/resources/model/UnbakedModel;)V"))
+    private static void replaceItemModelGenerator(ModelDiscovery instance, Identifier identifier, UnbakedModel model, Operation<Void> original) {
+        original.call(instance, identifier, new ImprovedItemModelBuilder());
+    }
+}
